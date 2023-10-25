502e5e9408c95501e4b0cc39097507eb03fc8ed2
GabrielBReis <gabriel.reissb@gmail.com>
2023-10-25 08:32:50 -0300

    .

---
+++ LP3-GerenciamentoDeFinancasPessoais/MyProject.MODEL/Saldo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProject.MODEL;
+
+public partial class Saldo
+{
+    public int Id { get; set; }
+
+    public double Valor { get; set; }
+
+    public int Idusuario { get; set; }
+
+    public virtual Usuario IdusuarioNavigation { get; set; } = null!;
+}
